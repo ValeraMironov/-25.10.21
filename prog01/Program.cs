1defdee475fa2045da96f3c016316692b752f353
valera mironov <valera4944690@mail.ru>
2021-10-25 22:14:28 +0300

    lesson01

---
+++ prog01/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+double a,b,c,d,S;
+Console.Write("a:");
+a=Convert.ToInt32(Console.ReadLine());
+Console.Write("b:");
+b=Convert.ToInt32(Console.ReadLine());
+Console.Write("c:");
+c=Convert.ToInt32(Console.ReadLine());
+Console.Write("d:");
+d=Convert.ToInt32(Console.ReadLine());
+S=(a+b+c+d)/4;
+Console.WriteLine(S);
